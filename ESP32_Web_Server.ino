8156b9eb623ac13b7836f364f2381fe073517d2d
Silas <103305606+SilasMilch@users.noreply.github.com>
2024-07-06 21:59:43 +0200

    Update ESP32_Web_Server.ino

--- ESP32_Web_Server.ino
+++ ESP32_Web_Server.ino
@@ -67,33 +67,4 @@ void handleSubmit() {
 }
 
 String getPage() {  //in getPage() kommt die Website rein mittels "page +="";"
-  String page = "<!DOCTYPE html>";
-  page += "<html>";
-  page += "<head>";
-  page += "<title>LED Control</title>";
-  page += "<style type=\"text/css\">";
-  page += "a img{ width: 25%; }";
-  page += "</style>";
-  page += "</head>";
-  page += "<body>";
-  page += "<h1>LED Control</h1>";
-  page += "<p>LED is currently: " + ledState + "</p>";
-  page += "<a href=\"/?LED=1\"><button>Turn On</button></a>";
-  page += "<a href=\"/?LED=0\"><button>Turn Off</button></a>";
-  page += "<div id=\"instafeed-container\"></div>";
-  page += "<script src=\"https://cdn.jsdelivr.net/gh/stevenschobert/instafeed.js@2.0.0rc1/src/instafeed.min.js\"></script>";
-  page += "<script type=\"text/javascript\">";
-  page += "var userFeed = new Instafeed({";
-  page += "get: 'user',";
-  page += "target: 'instafeed-container',";
-  page += "resolution: 'low_resolution',";
-  page += "sortBy: 'most-recent',";
-  page += "limit: 1,";
-  page += "accessToken: 'YOUR_INSTAGRAM_ACCESS_TOKEN_GOES_HERE'";
-  page += "});";
-  page += "userFeed.run();";
-  page += "</script>";
-  page += "</body>";
-  page += "</html>";
-  return page;
 }
